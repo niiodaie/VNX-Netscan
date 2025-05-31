609360fa1e3fedd4e32cfea35ac5dcef29a243af
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-05-31 01:07:02 -0500

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'vnx-netscan-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/assets/icon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
