ec1fc5abcdceb047b6f6527b360a1a2c547a61d7
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-23 02:46:30 -0500

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
-      <h1>SurpriseIdeas.com</h1>
-      <p>This is a Vercel-ready version. Frontend goes here.</p>
-    </div>
-  );
-}
