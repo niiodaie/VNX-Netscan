f7ada2afe6238d831621b22439f0613e22e8833d
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-24 14:04:44 -0500

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,11 +3,15 @@ import { createClient } from '@supabase/supabase-js'
 const url = import.meta.env.VITE_SUPABASE_URL as string
 const anon = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
+if (!url || !anon) {
+  throw new Error('supabaseUrl and supabaseAnon are required')
+}
+
 export const supabase = createClient(url, anon, {
-  auth: { 
-    persistSession: true, 
-    autoRefreshToken: true, 
-    detectSessionInUrl: true 
-  }
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true, // important for magic links
+  },
 })
 
