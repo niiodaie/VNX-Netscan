0e456624aa3c98fa4db1e1872fe8baed76de0038
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-25 18:18:12 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import AuthCallback from '@/pages/AuthCallback'
 
 // Layout Components
 import Header from '@/components/Header'
